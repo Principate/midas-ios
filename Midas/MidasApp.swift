20fc2ddfd76abe8c12f109b6215590a86ab64fde
Frodo2412 <brunolemus553@gmail>
2026-03-17 22:19:39 -0300

    Initial Commit

---
+++ Midas/MidasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidasApp.swift
+//  Midas
+//
+//  Created by Bruno Lemus on 17/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MidasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MidasTests/MidasTests.swift
@@ -0,0 +1,16 @@
+//
+//  MidasTests.swift
+//  MidasTests
+//
+//  Created by Bruno Lemus on 17/3/26.
+//
+
+import Testing
+
+struct MidasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
